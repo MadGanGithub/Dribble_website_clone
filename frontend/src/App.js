83bf84c1c9ab08748b1d3c467df7cb58521c7832
MadGanGithub <madhavganesan95@gmail.com>
2024-04-09 10:25:04 +0530

    removed unnecessary routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import Signup from "./pages/signup.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Profile from "./pages/profile.jsx";
-import Second from "./components/second_page.jsx";
 import Confirmation from "./components/confirmation.jsx";
-import First from "./components/first_page.jsx";
 
 function App() {
   return (
@@ -13,9 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/test" element={<Second />} />
           <Route path="/conf" element={<Confirmation />} />
-          <Route path="/sec" element={<First />} />
         </Routes>
       </Router>
     </>
